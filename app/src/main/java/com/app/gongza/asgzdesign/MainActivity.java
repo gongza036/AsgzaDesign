565d36f134f5c7a817950aad8870f5a12a001e88
gongzheng <fengyi036@163.com>
2016-10-22 00:41:39 +0800

    第一次提交

--- app/src/main/java/com/app/gongza/asgzdesign/MainActivity.java
+++ app/src/main/java/com/app/gongza/asgzdesign/MainActivity.java
@@ -1,15 +1,42 @@
 package com.app.gongza.asgzdesign;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.TextView;
 
 import com.app.gongza.asgzdesign.base.BaseAcitivity;
+import com.app.gongza.asgzdesign.tools.logger.L;
+
 
 public class MainActivity extends BaseAcitivity {
+    private TextView tv_first;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.e("Hello, %s ", "gongza");
+        init();
     }
+
+    private void init() {
+        tv_first = (TextView) findViewById(R.id.tv_first);
+        tv_first.setText("你好MacGit ");
+        tv_first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Snackbar mSnackbar = Snackbar.make(tv_first, "新Snackbar", Snackbar.LENGTH_LONG);
+                mSnackbar.show();
+                mSnackbar.setAction("取消", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        mSnackbar.dismiss();
+                        L.json("{\"name\":\"龚正\",\"age\":24}");
+                    }
+                });
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/app/gongza/asgzdesign/base/BaseAcitivity.java
+++ app/src/main/java/com/app/gongza/asgzdesign/base/BaseAcitivity.java
@@ -1,17 +1,24 @@
 package com.app.gongza.asgzdesign.base;
 
-import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 /**
  * Created by gongzheng on 2016/10/21.
  */
 
-public class BaseAcitivity extends Activity {
+public class BaseAcitivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
     }
 }
