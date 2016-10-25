d7ef23c15960ccac8c9a8f34e131cc94caa0f510
gongzheng <fengyi036@163.com>
2016-10-25 21:43:05 +0800

    加入一个LitePal数据库

---
+++ app/src/main/java/com/app/gongza/asgzdesign/models/AsdNews.java
@@ -0,0 +1,8 @@
+package com.app.gongza.asgzdesign.models;
+
+/**
+ * Created by gongzheng on 2016/10/25.
+ */
+
+public class AsdNews {
+}
--- app/src/main/java/com/app/gongza/libs/base/BaseApplication.java
+++ app/src/main/java/com/app/gongza/libs/base/BaseApplication.java
@@ -2,6 +2,8 @@ package com.app.gongza.libs.base;
 
 import android.app.Application;
 
+import org.litepal.LitePalApplication;
+
 /**
  * Created by gongzheng on 2016/10/21.
  */
@@ -11,5 +13,6 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        LitePalApplication.initialize(this);
     }
 }
