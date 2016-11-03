6a5c5c487c27d4ce9f760c5399b77836ed69f3f2
gongza <fengyi036@163.com>
2016-11-03 21:34:10 +0800

    增加了高斯模糊的例子

---
+++ app/src/main/java/com/app/gongza/asgzdesign/activities/demo/DemoAESactivity.java
@@ -0,0 +1,64 @@
+package com.app.gongza.asgzdesign.activities.demo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.app.gongza.asgzdesign.R;
+import com.app.gongza.libs.base.BaseAcitivity;
+import com.app.gongza.libs.tools.utils.Aes;
+
+/**
+ *  AES加密
+ */
+public class DemoAESActivity extends BaseAcitivity {
+    private EditText mInputET;
+    private TextView mShowEncryputTV;
+    private TextView mShowInputTV;
+    private static final String PASSWORD_STRING = "12345678";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        initView();
+    }
+
+    private void initView() {
+        mInputET = (EditText) findViewById(R.id.ase_input);
+        mShowEncryputTV = (TextView) findViewById(R.id.show_oringe_encrypt);
+        mShowInputTV = (TextView) findViewById(R.id.show_ase_encrypt);
+    }
+
+    /**
+     * 加密
+     *
+     * @param view
+     */
+    public void encrypt(View view) {
+        String inputString = mInputET.getText().toString().trim();
+        if (inputString.length() == 0) {
+            Toast.makeText(this, "请输入要加密的内容", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String encryStr = Aes.encrypt(PASSWORD_STRING, inputString);
+        mShowInputTV.setText(encryStr);
+    }
+
+    /**
+     * 解密
+     *
+     * @param view
+     */
+    public void decrypt(View view) {
+        String encryptString = mShowInputTV.getText().toString().trim();
+        if (encryptString.length() == 0) {
+            Toast.makeText(this, "解密字符串不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String decryStr = Aes.decrypt(PASSWORD_STRING, encryptString);
+        mShowEncryputTV.setText(decryStr);
+    }
+}
---
+++ app/src/main/java/com/app/gongza/asgzdesign/activities/demo/DemoBlurActivity.java
@@ -0,0 +1,44 @@
+package com.app.gongza.asgzdesign.activities.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.app.gongza.asgzdesign.MainActivity;
+import com.app.gongza.asgzdesign.R;
+import com.app.gongza.libs.tools.widget.BlurPopWin;
+
+public class DemoBlurActivity extends Activity {
+    private TextView tv_blur;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_blur);
+        initView();
+    }
+
+    private void initView() {
+        tv_blur= (TextView) findViewById(R.id.tv_blur);
+        tv_blur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new BlurPopWin.Builder(DemoBlurActivity.this).setContent("该配合你演出的我,眼视而不见,在比一个最爱你的人即兴表演")
+                        //Radius越大耗时越长,被图片处理图像越模糊
+                        .setRadius(3).setTitle("我是标题")
+                        //设置居中还是底部显示
+                        .setshowAtLocationType(0)
+                        .onClick(new BlurPopWin.PopupCallback() {
+                            @Override
+                            public void onClick(@NonNull BlurPopWin blurPopWin) {
+                                Toast.makeText(DemoBlurActivity.this, "中间被点了", Toast.LENGTH_SHORT).show();
+                                blurPopWin.dismiss();
+                            }
+                        }).show(tv_blur);
+            }
+        });
+    }
+}
